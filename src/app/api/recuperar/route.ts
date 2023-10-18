ae38eae85ba068609b215ac5fea3c2e059d677a0
martinapadovani <martinapadovani@gmail.com>
2023-10-18 11:27:19 -0300

    commit

--- src/app/api/recuperar/route.ts
+++ src/app/api/recuperar/route.ts
@@ -15,7 +15,9 @@ export async function POST(req:Request){
     const datos = await req.json()
     //Extraigo el email del usuario
 
-    const usuario = await prisma.usuario.findUnique({where: {email: datos.email}})
+    const usuario = await prisma.usuario.findUnique({
+        where: {email: datos.email}
+    })
     //Busco el usuario correspondiente al email que obtuvimos
 
     if(!usuario){//Si no existe un usuario con ese mail
@@ -24,22 +26,25 @@ export async function POST(req:Request){
         })
     }
 
-    //Si hay un usuario generamos un token unico para la recuperacion de la cuenta
-    const tokenUnico = sign(datos.email, process.env.TOKEN_SECRET as string, {})
-    //ejecutamos la funcion sign para crear un token con el email, encriptado con la variable de entorno TOKEN_SECRET
+    //Si hay un usuario generamos un token unico para la recuperacion de su cuenta
+    const tokenUnico = sign(
+        datos.email, 
+        process.env.TOKEN_SECRET as string, 
+        {}
+    )
+    /*  Ejecutamos la funcion sign para crear un token con el email, 
+        encriptado en base a variable de entorno 
+    */   
 
-
-    const emailAEnviar = { 
-        //Completamos los parametros de la plantilla
+    const parametrosPlantilla = { 
+        //Completamos los parametros para la plantilla del mail
         asunto: "Recupera tu contraseña",
         from_name: "Blog de Roberto",
         to_name: usuario.nombre,
-        link: `http://localhost:3000/auth/recuperar/${tokenUnico}`,
+        link: `http://localhost:3000/auth/recuperar/${tokenUnico}`, 
         user_email: datos.email
     }
 
-
-
-    return new Response(JSON.stringify(emailAEnviar))
-
+    return new Response(JSON.stringify(parametrosPlantilla))
 }
+
--- src/app/auth/recuperar/page.tsx
+++ src/app/auth/recuperar/page.tsx
@@ -1,17 +1,19 @@
 "use client"
 
 import { send } from "@emailjs/browser"
-import { FormEvent, useRef } from "react"
-
-
-
+import { FormEvent, useRef, useState } from "react"
 
 export default function Page(){
 
     const emailRef = useRef(null)
+    const [mailEnviado, setEnviado] = useState(false)
+
 
     async function recuperarContrasena(evento: FormEvent){
+        evento.preventDefault()
 
+        /*Hago una peticion a mi ruta del backend para enviarle el mail 
+         del usuario, y luego obtener la respuesta con los datos de la platilla */
         const respuesta = await fetch("http://localhost:3000/api/recuperar", {
             method: "POST",
             headers: {
@@ -19,32 +21,45 @@ export default function Page(){
             },
             //@ts-ignore
             body: JSON.stringify({email: emailRef.current?.value})
+            //Le enviamos el email que obtenemos desde el input del formulario
         })
-        //Obtengo la respuesta 
 
         if(respuesta.status != 200){
             alert ("Email no existe!")
+            return
         }
 
-        const email = await respuesta.json()
+        /*Obtenemos como respuesta de la peticion a nuestra ruta de back 
+        los parametros para la platilla */
+        const parametrosPlantilla = await respuesta.json()
 
-        //Ejecutamos send, apra enviar el mail de recuperacion
-        send("service_fpnggtv", //serviceID
+        //Ejecutamos send, para enviar el mail de recuperacion
+        send("service_x9zxxxo", //serviceID
             "template_6tuvt5h", // templateID
-            email, //parametros
+            parametrosPlantilla, //parametros
             "zEp9yRUw6VXdSQyUd"//Llave publica
-        )       
+        )      
         
+        setEnviado(true)
     }
 
     return (
-
         <section>
-            <form onSubmit={recuperarContrasena}>
+        { //Si el emailEnviado no es true (no se envio), va a mostrar el form
+            mailEnviado != true ? (
+
+            <form onSubmit={recuperarContrasena} className="text-black">
                 <input type="email" ref={emailRef} placeholder="Email de recuperacion"/>
 
-                <input className="text-white" type="submit" value="Enviar email de recuperacion" />
+                <input className="text-white" 
+                type="submit" 
+                value="Enviar email de recuperacion" />
             </form>
+
+            ) : ( // Si el mail se envio, cambio la interfaz por:
+                <h2>Mail enviado exitosamente! revisá tu correo</h2>
+            )
+        }
         </section>
     )
 
