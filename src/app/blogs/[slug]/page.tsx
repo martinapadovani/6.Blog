bd4e7d7291daaac71e43ad32a880885c0f084e3d
martinapadovani <martinapadovani@gmail.com>
2023-10-19 19:59:57 -0300

    commit

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,6 +1,59 @@
 import fs from "fs"
 import path from "path"
 import matter from "gray-matter"
-import { MDXRemote } from "next-mdx-remote"
+import { MDXRemote } from "next-mdx-remote/rsc"
 
-//generar estaticamente los blogs
+//Generar estaticamente los blogs
+export async function generateStaticParams(){
+
+    //Obtengo los archivos dentro de /blogs
+    const archivos = fs.readdirSync(path.join("src/blogs"))
+
+    //Creo la ruta de cada archivo
+    const rutas = archivos.map((nombreDeArchivo) => ({
+
+        slug: nombreDeArchivo.replace(".mdx", "")
+        //por cada archivo, devuelvo su slug ,que es igual al nombre del archivo sin el .mdx
+    }))
+
+    return rutas
+}
+
+//Funcion que lea los blogs 
+function obtenerBlog ({slug}: {slug: string}){
+    //recibe por parametro el slug para buscar el blog correspondiente
+
+    //Referenciamos el archivo del blog
+    const archivoDeBlog = fs.readFileSync(path.join("src/blogs/" + slug + ".mdx"), "utf-8")
+    //Busco el archivo del blog, dentro de "blogs" que tenga como nombre el slug.mdx
+
+    //Leemos el archivo referenciado y obtenemos sus metadatos y contenido
+    const {data: frontMatter, content } = matter(archivoDeBlog)
+
+    return { //la funcion devuelve un objeto (el blog) con:
+
+        frontMatter, //Los metadatos
+        slug,
+        content
+    }
+}
+
+//Creo una pagina que recibe el slug por parametro, para crear una pagina por cada blog referenciado
+export default function Page({params} : {params: {slug: string}}) {
+
+    //Ejecutamos obtenerBlog pasandole por parametro el slug obtenido desde la ruta de la pagina
+    const blog = obtenerBlog(params)    
+
+    return(
+        <article className="prose prose-sm md:prose-base lg:prose-lg prose-slate !prose-invert mx-auto">
+            <h1>{blog.frontMatter.title}</h1>
+            
+            {/*@ts-ignore*/}
+            <MDXRemote source={blog.content}></MDXRemote>
+            {/* MDXRemote renderiza el contenido MDX directamente en la app*/}
+        </article>
+    )
+}
+
+
+
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,51 +1,14 @@
-import Image from 'next/image'
-import fs from "fs"
-import path from "path"
-import matter from "gray-matter"
+
 import Link from 'next/link'
+import ListaDeBlogs from '@/components/ListaDeBlogs'
 
 export default function Home() {
 
-  //Guardo en una variable la ubicacion de la carpeta que contiene los blogs
-  const direccionDeMisBlogs = "src/blogs"
-
-  //Leer el contenido de la carpeta /blogs y guardar los nombres de sus archivos en un arreglo
-  const archivos = fs.readdirSync(path.join(direccionDeMisBlogs))
-
-  //Leer los archivos
-  const blogs = archivos.map((archivo => {
-    //mapeo mi arreglo con los archivos mdx donde por cada uno:
-
-    //Leo el contenido de un archivo en formato UTF-8 que se encuentra en la carpeta "blogs"
-    const contenidoDelArchivo = fs.readFileSync(path.join(direccionDeMisBlogs, archivo), "utf-8") 
-
-    //Analizo los metadatos delanteros del archivo y los extraigo en la variable frontMatter   
-    const { data: frontMatter } = matter(contenidoDelArchivo)
-
-    return { //Devuelvo el objeto con
-      meta: frontMatter, //atributo meta igual a los metadatos obtenidos
-      slug: archivo.replace(".mdx", "")
-      //texto que viene después del nombre de dominio y hace parte del enlace que conduce hacia el blog
-    }
-  }))
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/auth/registrarse" > Ir a registrarse</Link>
-      
-      <h2>Blog de Roberto</h2>
+      <Link href="/auth/registrarse" > Ir a registrarse</Link>  
 
-      {//Itero todos los blogs para mostrarlos con un formato html indicado
-        blogs.map((blog, index) =>(
-          <Link key={index} href={`/blogs/${blog.slug}`}>
-            <article>
-              <h3>{blog.meta.title}</h3>
-              <p>{blog.meta.description}</p>
-            </article>
-          </Link>
-        ))
-      }
-  
+      <ListaDeBlogs/>
     </main>
   )
 }
