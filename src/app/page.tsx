693976da9cb40eca790784dd88dacae1b412f572
martinapadovani <martinapadovani@gmail.com>
2023-10-19 15:41:13 -0300

    actualizacion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,23 +6,26 @@ import Link from 'next/link'
 
 export default function Home() {
 
+  //Guardo en una variable la ubicacion de la carpeta que contiene los blogs
   const direccionDeMisBlogs = "src/blogs"
 
+  //Leer el contenido de la carpeta /blogs y guardar los nombres de sus archivos en un arreglo
   const archivos = fs.readdirSync(path.join(direccionDeMisBlogs))
-  //Devuelve un arreglo de los nombre de los archivos dentro de ese directorio
 
-  console.log(archivos)
+  //Leer los archivos
+  const blogs = archivos.map((archivo => {
+    //mapeo mi arreglo con los archivos mdx donde por cada uno:
 
-  const blogs = archivos.map((nombreDelArchivo => {
-    const contenidoDelArchivo = fs.readFileSync(path.join(direccionDeMisBlogs, nombreDelArchivo), "utf-8")
+    //Leo el contenido de un archivo en formato UTF-8 que se encuentra en la carpeta "blogs"
+    const contenidoDelArchivo = fs.readFileSync(path.join(direccionDeMisBlogs, archivo), "utf-8") 
 
+    //Analizo los metadatos delanteros del archivo y los extraigo en la variable frontMatter   
     const { data: frontMatter } = matter(contenidoDelArchivo)
 
-    return {
-
-      meta: frontMatter,
-      slug: nombreDelArchivo.replace(".mdx", "")
-
+    return { //Devuelvo el objeto con
+      meta: frontMatter, //atributo meta igual a los metadatos obtenidos
+      slug: archivo.replace(".mdx", "")
+      //texto que viene después del nombre de dominio y hace parte del enlace que conduce hacia el blog
     }
   }))
 
@@ -31,13 +34,17 @@ export default function Home() {
       <Link href="/auth/registrarse" > Ir a registrarse</Link>
       
       <h2>Blog de Roberto</h2>
-      {/* 
-      {
-        blogs.map((blog, index => {
-          
 
-        }))
-      } */}
+      {//Itero todos los blogs para mostrarlos con un formato html indicado
+        blogs.map((blog, index) =>(
+          <Link key={index} href={`/blogs/${blog.slug}`}>
+            <article>
+              <h3>{blog.meta.title}</h3>
+              <p>{blog.meta.description}</p>
+            </article>
+          </Link>
+        ))
+      }
   
     </main>
   )
